427a588c2382f80314011aea24917ce6b8c59373
Geovou05 <geo05vou@gmail.com>
2026-07-25 20:15:07 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE = 'ultimate-car-wash-v15-login-final';
+const CACHE = 'ultimate-car-wash-v16-full-dark-fixed';
 const APP_SHELL = ['./','./index.html','./manifest.json','./icon.svg','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
 self.addEventListener('install', event => {
   event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
