d26915b1d503574e18455024f868ade2cb49d29c
Geovou05 <geo05vou@gmail.com>
2026-07-25 21:37:25 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,22 +1,24 @@
-const CACHE = 'ultimate-car-wash-v16-full-dark-fixed';
-const APP_SHELL = ['./','./index.html','./manifest.json','./icon.svg','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
-self.addEventListener('install', event => {
-  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
+const CACHE='ultimate-hand-carwash-v17-branded';
+const ASSETS=['./','./index.html','./manifest.json','./ultimate-logo.jpg','./brand-flyer.jpg','./icon-192.png','./icon-512.png','./apple-touch-icon.png','./favicon.png'];
+self.addEventListener('install',event=>{
+  self.skipWaiting();
+  event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(ASSETS)));
 });
-self.addEventListener('activate', event => {
-  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
+self.addEventListener('activate',event=>{
+  event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));
 });
-self.addEventListener('fetch', event => {
-  if (event.request.method !== 'GET') return;
-  const url = new URL(event.request.url);
-  if (event.request.mode === 'navigate' || url.pathname.endsWith('/index.html')) {
-    event.respondWith(fetch(event.request).then(response => {
-      const copy=response.clone(); caches.open(CACHE).then(c=>c.put('./index.html',copy)); return response;
-    }).catch(() => caches.match('./index.html')));
+self.addEventListener('fetch',event=>{
+  if(event.request.method!=='GET')return;
+  if(event.request.mode==='navigate'){
+    event.respondWith(fetch(event.request).then(response=>{
+      const copy=response.clone();caches.open(CACHE).then(cache=>cache.put('./index.html',copy));return response;
+    }).catch(()=>caches.match('./index.html')));
     return;
   }
-  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
-    if(url.origin===location.origin){const copy=response.clone();caches.open(CACHE).then(c=>c.put(event.request,copy));}
+  event.respondWith(caches.match(event.request).then(cached=>cached||fetch(event.request).then(response=>{
+    if(response&&response.status===200&&response.type==='basic'){
+      const copy=response.clone();caches.open(CACHE).then(cache=>cache.put(event.request,copy));
+    }
     return response;
   })));
 });
