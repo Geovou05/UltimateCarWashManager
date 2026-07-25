c06e94fff1faff57633be888166c077a3e86b2d9
Geovou05 <geo05vou@gmail.com>
2026-07-25 19:26:34 +0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,27 +1,22 @@
-const C="ucwm-v7-final-sync";
-const F=["./","./index.html","./manifest.json","./icon.svg"];
-
-self.addEventListener("install",e=>{
-  self.skipWaiting();
-  e.waitUntil(caches.open(C).then(c=>c.addAll(F)));
+const CACHE = 'ultimate-car-wash-v13';
+const APP_SHELL = ['./','./index.html','./manifest.json','./icon.svg','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
+self.addEventListener('install', event => {
+  event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(APP_SHELL)).then(() => self.skipWaiting()));
 });
-
-self.addEventListener("activate",e=>{
-  e.waitUntil(
-    caches.keys()
-      .then(keys=>Promise.all(keys.filter(k=>k!==C).map(k=>caches.delete(k))))
-      .then(()=>self.clients.claim())
-  );
+self.addEventListener('activate', event => {
+  event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))).then(() => self.clients.claim()));
 });
-
-self.addEventListener("fetch",e=>{
-  e.respondWith(
-    fetch(e.request)
-      .then(r=>{
-        const copy=r.clone();
-        caches.open(C).then(c=>c.put(e.request,copy));
-        return r;
-      })
-      .catch(()=>caches.match(e.request))
-  );
+self.addEventListener('fetch', event => {
+  if (event.request.method !== 'GET') return;
+  const url = new URL(event.request.url);
+  if (event.request.mode === 'navigate' || url.pathname.endsWith('/index.html')) {
+    event.respondWith(fetch(event.request).then(response => {
+      const copy=response.clone(); caches.open(CACHE).then(c=>c.put('./index.html',copy)); return response;
+    }).catch(() => caches.match('./index.html')));
+    return;
+  }
+  event.respondWith(caches.match(event.request).then(cached => cached || fetch(event.request).then(response => {
+    if(url.origin===location.origin){const copy=response.clone();caches.open(CACHE).then(c=>c.put(event.request,copy));}
+    return response;
+  })));
 });
